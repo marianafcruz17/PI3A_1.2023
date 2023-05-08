fda8ec67f8e46c1badec98df2e5d90ee6d630039
Paulo <dpseutroco@gmail.com>
2023-05-07 21:39:16 -0300

    Semi pronto

--- lib/src/feature/home/view/widget/home.dart
+++ lib/src/feature/home/view/widget/home.dart
@@ -1,55 +1,60 @@
 import 'package:flutter/material.dart';
 import 'package:marmita_social/src/feature/home/view/widget/cardapio.dart';
 
-
 class UserHome extends StatelessWidget {
   const UserHome({super.key});
 
   @override
   Widget build(BuildContext context) {
-   return Scaffold(
-    body: Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        SizedBox(height: 20 , width: 50,),
-        ElevatedButton(
-          onPressed: () {
-            Navigator.push(
-            context,
-            MaterialPageRoute(builder: (context) => Cardapio()),
-          );
-          },
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Image.asset(
-                'lib/assets/images/alimentos.png',
-                width: 30,
-                height: 30,
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          SizedBox(
+            height: 230,
+            width: 200,
+            child: ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const Cardapio()),
+                );
+              },
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Image.asset(
+                    'lib/assets/images/alimentos.png',
+                    width: 180,
+                    height: 200,
+                  ),
+                  const SizedBox(width: 10),
+                  const Text('Cardápio'),
+                ],
               ),
-              SizedBox(width: 10),
-              Text('Cardápio'),
-            ],
+            ),
           ),
-        ),
-        SizedBox(height: 10,  width: 50,),
-        TextButton(
-          onPressed: () {},
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Image.asset(
-                'lib/assets/images/localização.png',
-                width: 30,
-                height: 30,
+          SizedBox(
+            height: 230,
+            width: 200,
+            child: ElevatedButton(
+              onPressed: () {},
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Image.asset(
+                    'lib/assets/images/localização.png',
+                    width: 180,
+                    height: 200,
+                  ),
+                  const SizedBox(width: 10),
+                  const Text('Restaurantes Próximos'),
+                ],
               ),
-              SizedBox(width: 10),
-              Text('Restaurantes Próximos'),
-            ],
+            ),
           ),
-        ),
-      ],
-    ),
-  );
+        ],
+      ),
+    );
   }
 }
